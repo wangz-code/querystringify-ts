b0e04d8cc2d52e9ad9c738250d848277cbaaedaf
wangqz <2809755637@qq.com>
2022-03-06 19:38:26 +0800

    1.0.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,12 +4,11 @@
  * @param {String} query The query string that needs to be parsed.
  * @returns {Object}
  * @api public
- * @description
- *  qs.parse('?foo=bar');         // { foo: 'bar' }
-    qs.parse('#foo=bar');         // { foo: 'bar' }
-    qs.parse('foo=bar');          // { foo: 'bar' }
-    qs.parse('foo=bar&bar=foo');  // { foo: 'bar', bar: 'foo' }
-    qs.parse('foo&bar=foo');      // { foo: '', bar: 'foo' }
+ * @description qs.parse('?foo=bar');         // { foo: 'bar' }
+ * @description qs.parse('#foo=bar');         // { foo: 'bar' }
+ * @description qs.parse('foo=bar');          // { foo: 'bar' }
+ * @description qs.parse('foo=bar&bar=foo');  // { foo: 'bar', bar: 'foo' }
+ * @description qs.parse('foo&bar=foo');      // { foo: '', bar: 'foo' }
  */
 declare function querystring(query: string): {
     [k: string]: any;
@@ -18,8 +17,12 @@ declare function querystring(query: string): {
  * Transform a query string to an object.
  *
  * @param {Object} obj Object that should be transformed.
- * @param {String} prefix Optional prefix.
+ * @param {String} prefix Optional prefix. 分割前缀符号
  * @returns {String}
+ * @description qs.stringify({ foo: bar });       // foo=bar
+ * @description qs.stringify({ foo: bar }, true); // ?foo=bar
+ * @description qs.stringify({ foo: bar }, '#');  // #foo=bar
+ * @description qs.stringify({ foo: '' }, '&');   // &foo=
  * @api public
  */
 declare function querystringify(obj: {
--- dist/index.js
+++ dist/index.js
@@ -37,12 +37,11 @@ function encode(input) {
  * @param {String} query The query string that needs to be parsed.
  * @returns {Object}
  * @api public
- * @description
- *  qs.parse('?foo=bar');         // { foo: 'bar' }
-    qs.parse('#foo=bar');         // { foo: 'bar' }
-    qs.parse('foo=bar');          // { foo: 'bar' }
-    qs.parse('foo=bar&bar=foo');  // { foo: 'bar', bar: 'foo' }
-    qs.parse('foo&bar=foo');      // { foo: '', bar: 'foo' }
+ * @description qs.parse('?foo=bar');         // { foo: 'bar' }
+ * @description qs.parse('#foo=bar');         // { foo: 'bar' }
+ * @description qs.parse('foo=bar');          // { foo: 'bar' }
+ * @description qs.parse('foo=bar&bar=foo');  // { foo: 'bar', bar: 'foo' }
+ * @description qs.parse('foo&bar=foo');      // { foo: '', bar: 'foo' }
  */
 function querystring(query) {
     var parser = /([^=?#&]+)=?([^&]*)/g;
@@ -68,8 +67,12 @@ function querystring(query) {
  * Transform a query string to an object.
  *
  * @param {Object} obj Object that should be transformed.
- * @param {String} prefix Optional prefix.
+ * @param {String} prefix Optional prefix. 分割前缀符号
  * @returns {String}
+ * @description qs.stringify({ foo: bar });       // foo=bar
+ * @description qs.stringify({ foo: bar }, true); // ?foo=bar
+ * @description qs.stringify({ foo: bar }, '#');  // #foo=bar
+ * @description qs.stringify({ foo: '' }, '&');   // &foo=
  * @api public
  */
 function querystringify(obj, prefix) {
